c2089870d8b55e5492ff3cc656a3cc3548696e4f
Javier Rodríguez <134724418+javierrdlp@users.noreply.github.com>
2024-10-23 18:33:37 +0000

    Creación de la funcion domanGenerator

---
+++ app.js
@@ -0,0 +1,21 @@
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension =['.com', '.net', '.us', '.io'];
+
+function domainGenerator(pronoun, adj, noun, extension){     
+
+    for(let i = 0; i < pronoun.length; i++){       
+        for(let j = 0; j < adj.length; j++){
+            for(let k = 0; k < noun.length; k++){
+                for(let l = 0; l < extension.length; l++){
+                    let domain = pronoun[i] + adj[j] + noun[k] + extension[l];
+                    console.log(domain);
+                }               
+            }            
+        }        
+    }  
+}
+
+domainGenerator(pronoun, adj, noun, extension);
